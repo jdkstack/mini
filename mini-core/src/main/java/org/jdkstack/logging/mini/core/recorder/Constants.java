7a729b228f6e54662221c8f3bd793223a8eb19cd
admin <jdkstack@gmail.com>
2023-09-22 09:48:41 +0800

    .

---
+++ mini-core/src/main/java/org/jdkstack/logging/mini/core/recorder/Constants.java
@@ -0,0 +1,20 @@
+package org.jdkstack.logging.mini.core.recorder;
+
+/**
+ * This is a class description.
+ *
+ * <p>.
+ *
+ * <p>Another description after blank line.
+ *
+ * @author admin
+ */
+public final class Constants {
+
+  /** . */
+  public static final String SYSTEM = "system";
+
+  private Constants() {
+    //
+  }
+}
