b37bb18ec3432016deef6a521b7ce3ff996b4067
admin <jdkstack@gmail.com>
2023-12-24 13:38:13 +0800

    删除实验性特性代码.

--- mini-extension/src/main/java/org/jdkstack/logging/mini/extension/plugin/rest/RestPlugin.java
+++
@@ -1,7 +0,0 @@
-package org.jdkstack.logging.mini.extension.plugin.rest;
-
-import org.jdkstack.logging.mini.extension.plugin.Plugin;
-
-public interface RestPlugin extends Plugin {
-  //
-}
--- mini-jmh/src/main/java/test/pattern/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 这是一个日期事件工具.
- *
- * <p>满足ISO8601的一部分规范，此工具为日志框架量身定做。
- *
- * @author admin
- */
-package test.pattern;
