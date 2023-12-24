3e45fb0360132963e2ba00ee935f058fa2aac241
admin <jdkstack@gmail.com>
2023-12-24 13:38:13 +0800

    删除实验性特性代码.

--- mini-extension/src/main/java/org/jdkstack/logging/mini/extension/web/annotation/RequestMethod.java
+++
@@ -1,5 +0,0 @@
-package org.jdkstack.logging.mini.extension.web.annotation;
-
-public enum RequestMethod {
-  GET, POST
-}
--- mini-jmh/src/main/java/test/pattern/ArrayPatternConverter.java
+++
@@ -1,6 +0,0 @@
-package test.pattern;
-
-public interface ArrayPatternConverter extends PatternConverter {
-
-  void format(final StringBuilder toAppendTo, Object... objects);
-}
