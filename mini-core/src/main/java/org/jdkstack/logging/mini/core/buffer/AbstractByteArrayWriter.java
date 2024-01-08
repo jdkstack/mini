bd51737ded4d0e314b524da19105bdf253fec9ed
admin <jdkstack@gmail.com>
2024-01-08 15:09:44 +0800

    base64.

--- mini-core/src/main/java/org/jdkstack/logging/mini/core/buffer/AbstractByteArrayWriter.java
+++ mini-core/src/main/java/org/jdkstack/logging/mini/core/buffer/AbstractByteArrayWriter.java
@@ -2,6 +2,10 @@ package org.jdkstack.logging.mini.core.buffer;
 
 import java.nio.ByteBuffer;
 import org.jdkstack.logging.mini.api.buffer.ByteWriter;
+import org.jdkstack.logging.mini.api.lz4.Encoder;
+import org.jdkstack.logging.mini.core.lz4.Base64;
+import org.jdkstack.logging.mini.core.thread.LogConsumeThread;
+import org.jdkstack.logging.mini.core.tool.ThreadLocalTool;
 
 /**
  * .
@@ -40,13 +44,22 @@ public abstract class AbstractByteArrayWriter implements ByteWriter {
   @Override
   public final void flush(final ByteBuffer buf) throws Exception {
     buf.flip();
+    final LogConsumeThread logConsumeThread = ThreadLocalTool.getLogConsumeThread();
+    final ByteBuffer out = logConsumeThread.getOut();
+    final ByteBuffer out2 = logConsumeThread.getOut2();
+    Encoder encoderLz4 = logConsumeThread.getEncoderLz4();
+    encoderLz4.encode(buf, out);
+    out.flip();
+    Base64.encode(out, out2);
     try {
       // 如果有数据.
       if (0 < buf.remaining()) {
         // 开始写数据.
-        this.writeToDestination(buf.array(), buf.arrayOffset() + buf.position(), buf.remaining());
+        this.writeToDestination(out2.array(), out2.arrayOffset() + out2.position(), out2.remaining());
       }
     } finally {
+      out.clear();
+      out2.clear();
       // 清除缓存.
       buf.clear();
     }
