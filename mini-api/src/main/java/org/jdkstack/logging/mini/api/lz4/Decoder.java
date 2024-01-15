1ad469614ff6c9c5b28ff1ca2d0ecaad51307f75
admin <jdkstack@gmail.com>
2024-01-15 16:35:58 +0800

    base64.

--- mini-api/src/main/java/org/jdkstack/logging/mini/api/lz4/Decoder.java
+++ mini-api/src/main/java/org/jdkstack/logging/mini/api/lz4/Decoder.java
@@ -2,7 +2,7 @@ package org.jdkstack.logging.mini.api.lz4;
 
 import java.nio.ByteBuffer;
 
-public interface Lz4Decoder {
+public interface Decoder {
 
   void decode(ByteBuffer input, ByteBuffer output);
 }
